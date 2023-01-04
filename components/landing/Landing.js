b9220f8c895021129694ac473a451cdd0797627f
Teemu Väyrynen <85222095+teemuvayrynen@users.noreply.github.com>
2023-01-04 13:59:34 +0200

    image fixing

--- components/landing/Landing.js
+++ components/landing/Landing.js
@@ -45,6 +45,8 @@ const Img = styled.div`
   display: ${props => props.width <= 800 ? 'none' : 'block'};
   width: 500px;
   height: 500px;
+  min-height: 500px;
+  min-width: 500px;
   animation-duration: 4s;
   animation-iteration-count: infinite;
   
