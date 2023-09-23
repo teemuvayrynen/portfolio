a1e9b2bd6fd235b5a146604d04cfc3d40b7270a2
Teemu Väyrynen <teemu@vayrynen.fi>
2023-09-23 13:59:09 +0300

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,6 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <Component {...pageProps} />
-      <Analytics/>
     </>
   )
 }
